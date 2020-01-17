e72cbf062b1b99a4717584bb47ab00aad212afce
Alex Rupp-Coppi <alex@dossamer.io>
2020-01-17 17:26:35 -0500

    added react-redux, redux, react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// this tutorial (while dated in some ways that broke things) was super helpful: 
+// https://www.freecodecamp.org/news/part-1-react-app-from-scratch-using-webpack-4-562b1d231e75/
+
 const Index = () => {
   return <div>Hello React!</div>;
 };
