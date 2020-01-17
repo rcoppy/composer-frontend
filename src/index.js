a6b0de2fdbabd0de88a1e97c4641cc9a2fa190fb
Alex Rupp-Coppi <alex@dossamer.io>
2020-01-17 17:54:42 -0500

    app component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import App from "./components/app.js";
+
 // this tutorial (while dated in some ways that broke things) was super helpful: 
 // https://www.freecodecamp.org/news/part-1-react-app-from-scratch-using-webpack-4-562b1d231e75/
 
-const Index = () => {
-  return <div>Hello React!</div>;
-};
-
-ReactDOM.render(<Index />, document.getElementById("index"));
+ReactDOM.render(<App />, document.getElementById("root"));
