a46a1e559798280caf80cc06fc81df8971003bb8
Alex Rupp-Coppi <alex@dossamer.io>
2020-01-18 09:38:23 -0500

    added calculator icon

--- src/components/app.js
+++ src/components/app.js
@@ -5,11 +5,14 @@ import Footer from './pug/footer';
 import Navbar from './pug/navbar';
 import CalculatorPads from './pug/calculator_pads';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalculator } from '@fortawesome/free-solid-svg-icons';
+
 // import '../../assets/sass/main.scss';
 
 const App = () => pug`
   Header(
-    content=${pug`h1 Best Calculator`})
+    content=${pug`h1 Best Calculator #[FontAwesomeIcon(icon=faCalculator)]`})
 
   ContentMixin
     h2 Hello! Welcome to calculator
