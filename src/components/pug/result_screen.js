b81c86ad98afbae49e277282d0eabafc9115047d
Alex Rupp-Coppi <alex@dossamer.io>
2020-01-18 14:40:24 -0500

    hook to result screen

--- src/components/pug/result_screen.js
+++ src/components/pug/result_screen.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const ResultScreen = props => pug`
+const ResultScreen = props => {
+    
+    const result = useSelector(state => state.currentResult);
+    
+    return (pug`
     .result-screen
-      span 10345
-`;
+      span = result
+`)};
 
 export default ResultScreen;
