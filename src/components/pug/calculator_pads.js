b03c438b5421e12e7c6d6d1778d447c6868e866c
Alex Rupp-Coppi <alex@dossamer.io>
2020-01-18 10:21:08 -0500

    result screen added

--- src/components/pug/calculator_pads.js
+++ src/components/pug/calculator_pads.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NumberPad from './number_pad';
 import OperatorPad from './operator_pad';
+import ResultScreen from './result_screen';
 
 const CalculatorPads = () => pug`
     .calculator-pads
       NumberPad
       OperatorPad
+      ResultScreen
 `;
 
 export default CalculatorPads;
---
+++ src/components/pug/result_screen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ResultScreen = props => pug`
+    .result-screen
+      span 10345
+`;
+
+export default ResultScreen;
