c16ace84366973560d77ee50397193bc1223a511
Alex Rupp-Coppi <alex@dossamer.io>
2020-01-26 18:50:49 -0500

    updated navbar comments

--- src/components/pug/navbar.js
+++ src/components/pug/navbar.js
@@ -4,7 +4,7 @@ const Navbar = ({children}) => pug`
   .navbar
     .container
       = children
-      // intended use is span #[a(href="destination") Link]
+      // intended use is a(href="destination") Link
 `;
 
 export default Navbar;
