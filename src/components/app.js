8f57d1e4ac5927f8269fdd801fbb8c61855fa7f0
Alex Rupp-Coppi <alex@dossamer.io>
2020-01-26 18:45:46 -0500

    basic routing, styling

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
 import Header from './pug/header';
 import ContentMixin from './pug/content_mixin'
 import Footer from './pug/footer';
@@ -10,20 +19,33 @@ import { faCalculator } from '@fortawesome/free-solid-svg-icons';
 // import '../../assets/sass/main.scss';
 
 const App = () => pug`
-  Header(
-    content=${pug`h1 Best Calculator #[FontAwesomeIcon(icon=faCalculator)]`})
+  Router
+    Header(
+      content=${pug`h1 Best Calculator #[FontAwesomeIcon(icon=faCalculator)]`}
+      navLinks=${pug`
+        Link(to="/") Home
+        Link(to="/about") About
+        Link(to="/topics") Topics
+      `})
 
-  ContentMixin
-    h2 *beep boop*
+    ContentMixin
+      h2 *beep boop*
+      Switch 
+        Route(path="/about")
+          h3 Welcome to about
+        Route(path="/topics")
+          h3 Welcome to topics
+        Route(path="/")
+          h3 Welcome to home
 
-  Footer
-    p by #[a(href="http://alexrupp.com/") Alex], Jan 2020
-    p.mb-0 Using!
-    ul
-      li react-bootstrap
-      li fontawesome
-      li google fonts
-      li subtlepatterns
+    Footer
+      p by #[a(href="http://alexrupp.com/") Alex], Jan 2020
+      p.mb-0 Using!
+      ul
+        li react-bootstrap
+        li fontawesome
+        li google fonts
+        li subtlepatterns
 `;
 
 export default App;
