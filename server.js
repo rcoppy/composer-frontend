c971f888c10425b2aae82266590f775edf02dce7
Alexander Rupp-Coppi <amr2311@columbia.edu>
2020-01-27 15:34:45 -0500

    upgraded to express server; fixed some broken image bundling

--- server.js
+++ server.js
@@ -1,72 +1,24 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 9000;
 
-/*
-    FYI this simple server is derived from boilerplate code I picked up eons ago that's been hopping from 
-    project to project; I unfortunately have no idea which tutorial/documentation quickstart resource it originally
-    came from. (My hunch is the folks who maintain the http package.)
-*/
+const app = express();
 
-http.createServer(function (req, res) {
-    console.log(`${req.method} ${req.url}`);
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'dist')));
 
-    // hacky redirect
-    if (req.url === '/') {
-        res.writeHead(301, { Location: '/dist/index.html' });
-        res.end();
-    }
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
 
-    // parse URL
-    const parsedUrl = url.parse(req.url);
-    // extract URL path
-    let pathname = `.${parsedUrl.pathname}`;
-    // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-    const ext = path.parse(pathname).ext;
-    // maps file extention to MIME typere
-    const map = {
-        '.ico': 'image/x-icon',
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.json': 'application/json',
-        '.css': 'text/css',
-        '.png': 'image/png',
-        '.jpg': 'image/jpeg',
-        '.wav': 'audio/wav',
-        '.mp3': 'audio/mpeg',
-        '.ogg': 'audio/ogg',
-        '.svg': 'image/svg+xml',
-        '.pdf': 'application/pdf',
-        '.doc': 'application/msword'
-    };
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
-    fs.exists(pathname, function (exist) {
-        if (!exist) {
-            // if the file is not found, return 404
-            res.statusCode = 404;
-            res.end(`File ${pathname} not found!`);
-            return;
-        }
+const port = process.env.PORT || 5000;
+app.listen(port);
 
-        // if is a directory search for index file matching the extention
-        if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
-
-
-        // read file from file system
-        fs.readFile(pathname, function (err, data) {
-            if (err) {
-                res.statusCode = 500;
-                res.end(`Error getting the file: ${err}.`);
-            } else {
-                // if the file is found, set Content-type and send data
-
-                res.setHeader('Content-type', map[ext] || 'text/plain');
-                res.end(data);
-            }
-        });
-    });
-}).listen(parseInt(port));
-
-console.log(`Server listening on port ${port}`);
+console.log('App is listening on port ' + port);
--- src/components/pug/content/dummy_gallery_card.js
+++ src/components/pug/content/dummy_gallery_card.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import GalleryCard from './gallery_card';
+import AvatarImage from '../../../../assets/images/default_avatar.jpg';
+import ScoreImage from '../../../../assets/images/score_large.jpg';
 
 const DummyGalleryCard = () => pug`
     GalleryCard(score_title='Forest Symphony', 
                 author_name='Brahms', 
-                img_src='../../../assets/images/score_large.jpg', 
+                img_src=ScoreImage, 
                 score_url='dummy_score_page.pug', 
                 author_url='dummy_author_page.pug', 
-                avatar_src='../../../assets/images/default_avatar.jpg')
+                avatar_src=AvatarImage)
 `;
 
 export default DummyGalleryCard;
