2452e56f4d8f3ff47160ad3c6c0ea0268fd7408e
Alexander Rupp-Coppi <amr2311@columbia.edu>
2020-02-19 20:57:13 -0500

    updating references to store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import {Provider} from "react-redux";
-import store from "./redux/store";
+import { store } from './redux/helpers';
 
 import App from "./components/app.js";
 
