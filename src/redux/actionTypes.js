edfc06b9159f497894dc0edd6e2980ab8340d0b4
Alexander Rupp-Coppi <amr2311@columbia.edu>
2020-02-19 19:43:44 -0500

    clearing out calculator scaffolding

--- src/redux/actionTypes.js
+++
@@ -1,8 +0,0 @@
-export const APPEND_DIGIT = "APPEND_DIGIT";
-export const APPEND_DECIMAL = "APPEND_DECIMAL";
-export const DELETE_DIGIT = "DELETE_DIGIT";
-export const ERASE_SCREEN = "ERASE_SCREEN";
-export const DELETE_CACHE = "DELETE_CACHE";
-export const CACHE_RESULT = "CACHE_RESULT";
-export const LOAD_CACHE = "LOAD_CACHE";
-export const QUEUE_OPERATION = "QUEUE_OPERATION";
--- src/redux/constants.js
+++
@@ -1,7 +0,0 @@
-export const OPERATORS = {
-    MULTIPLY: "multiply",
-    DIVIDE: "divide",
-    ADD: "add",
-    SUBTRACT: "subtract",
-    COMPUTE: "compute"
-  };
