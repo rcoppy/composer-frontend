b0f8f3a2e28d4fce3e54d28af98aabc5d78fb46e
Alexander Rupp-Coppi <amr2311@columbia.edu>
2020-02-19 19:48:41 -0500

    adding auth functionality

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,13 @@
-import { combineReducers } from "redux";
-import session from "./session";
+import { combineReducers } from 'redux';
 
-export default combineReducers({ session });
+import { authentication } from './authentication.reducer';
+import { users } from './users.reducer';
+import { alert } from './alert.reducer';
+
+const rootReducer = combineReducers({
+  authentication,
+  users,
+  alert
+});
+
+export default rootReducer;
